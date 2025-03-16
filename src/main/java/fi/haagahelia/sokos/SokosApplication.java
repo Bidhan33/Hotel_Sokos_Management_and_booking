16d608862806929907469b387b0b8cc499f6ad83
bidan <bidhan.adhikari@myy.haaga-helia.fi>
2025-03-16 21:36:36 +0200

    Initial commit for Hotel Sokos Management and Booking project

---
+++ src/main/java/fi/haagahelia/sokos/SokosApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.sokos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SokosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/sokos/SokosApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.sokos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SokosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
